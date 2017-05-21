8e9740f002649b2657554e571fac96aff0aa7312
imran <sajid506a@gmail.com>
2017-05-21 19:08:09 +0530

    changes

--- js/index.js
+++ js/index.js
@@ -11,39 +11,40 @@ var Hello = React.createClass({
 });
 
 var Slider = React.createClass({
-   getInitialState:function () {
-     return{
-         currentSlide:0,
-         slides:[
-             {title:"Title1",content:"Content#1"},
-             {title:"Title2",content:"Content#2"},
-             {title:"Title3",content:"Content#3"},
-             {title:"Title4",content:"Content#4"}
-         ]
-     }
-   },
+    getInitialState:function () {
+        return{
+            currentSlide:0,
+            slides:[
+                {title:"Title1",content:"Content#1"},
+                {title:"Title2",content:"Content#2"},
+                {title:"Title3",content:"Content#3"},
+                {title:"Title4",content:"Content#4"}
+            ]
+        }
+    },
     nextClick:function () {
-      if(this.state.currentSlide===this.state.slides.length-1){
-          this.setState({currentSlide:0})
-      }
-      else{
-          this.setState({currentSlide:this.state.currentSlide+1})
-      }
+        if(this.state.currentSlide===this.state.slides.length-1){
+            this.setState({currentSlide:0})
+        }
+        else{
+            this.setState({currentSlide:this.state.currentSlide+1})
+        }
     },
     render: function () {
         var slide = this.state.slides[this.state.currentSlide];
         return (
             <div className="col-md-10">
-                <div className="panel panel-default">
-                <h1>{slide.title}  </h1>
+                <div className="panel panel-success">
+                    <h1 className="panel-heading">{slide.title}  </h1>
 
-                </div>
-                <div className="panel-body jumbotron">{slide.content}</div>
+
+                <div className="panel-body">{slide.content}</div>
                 <button onClick={this.nextClick}>Next</button>
+                </div>
             </div>
         )
 
     }
 })
 
-React.renderComponent(<Slider />, document.getElementById("page"));
+React.renderComponent(<Slider />, document.getElementById("page"))
